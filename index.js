f759bce98a83ee8354c0c5d4c80ef5a9ee226e2d
Melissa Heying <melissakheying@gmail.com>
2020-05-15 16:54:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten("Ralph") {
-  console.log(destructivelyAppendKitten)
+function destructivelyAppendKitten() {
+  return destructivelyAppendKitten("Ralph")
 }
 
 
